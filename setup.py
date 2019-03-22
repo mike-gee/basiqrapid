13868ac4d98f0364e13a6374cdd58c805873c718
Mike Gee <michael.gee2018@gmail.com>
2019-03-22 14:09:33 +1100

    initial

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name=“basiqrapid”,
+    packages=["basiqrapid"],
+    version="1.0.0",
+    description="A Python wrapper for Basiq.io’s API",
+    author="Michael Gee",
+    python_requires=">=3",
+    install_requires=["requests","pandas"],
+    author_email="michael.gee2018@gmail.com",
+    url="https://github.com/mike-gee/basiqrapid",
+    download_url="https://github.com/mike-gee/basiqrapid/archive/v_01.tar.gz",
+    keywords=["basiq", "finance", "sdk", "api", "python", "wrapper"],
+    classifiers=[]
+)
